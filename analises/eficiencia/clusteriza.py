900b26b25bede7c943653bc720093100717d3caf
Saul Campos Berardo <camposb@tcu.gov.br>
2020-03-15 20:16:52 -0300

    Ajuste no diretório de saída de produtos da clusterização

--- analises/eficiencia/clusteriza.py
+++ analises/eficiencia/clusteriza.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 if __name__ == '__main__':
 
     #
-    ANO = '2019'
+    ANO = '2018'
 
     # Obtem diretório raiz do projeto
     DIRETORIO_RAIZ_PROJETO = os.path.dirname(os.path.realpath(__file__))
@@ -22,13 +22,14 @@ if __name__ == '__main__':
     DIRETORIO_DADOS_RESULTADOS = os.path.join(DIRETORIO_RAIZ_PROJETO, 'dados', 'resultados')
 
     # Carrega dados da planilha gerada pelo Eric
-    arquivo_para_clusterizacao = os.path.join(DIRETORIO_DADOS_INTERMEDIARIOS, 'perfil_hosp_pubs_oss_dez_{ANO}.xlsx'.format(ANO=ANO))
-    df_hosp_pubs_oss_com_perfil = pd.read_excel(arquivo_para_clusterizacao)
+    arquivo_para_clusterizacao = os.path.join(DIRETORIO_DADOS_INTERMEDIARIOS, 'hosp_pubs_{ANO}.xlsx'.format(ANO=ANO))
+    df_hosp_pubs = pd.read_excel(arquivo_para_clusterizacao, index_col='CNES')
 
-    # Cria df somente com as colunas utilizadas na clusterização (os subrupos começam na coluna 13)
-    colunas_para_clust = df_hosp_pubs_oss_com_perfil.columns[13::].to_list()
-    df_para_clust = df_hosp_pubs_oss_com_perfil[colunas_para_clust]
-    df_para_clust = df_para_clust.set_index(df_hosp_pubs_oss_com_perfil['CNES'])
+    # Cria df somente com as colunas utilizadas na clusterização (as colunas de procedimentos iniciam na SIA-0101)
+    idx_coluna_primeiro_proc = df_hosp_pubs.columns.to_list().index('SIA-0101')
+    colunas_para_clust = df_hosp_pubs.columns[idx_coluna_primeiro_proc::].to_list()
+    df_para_clust = df_hosp_pubs[colunas_para_clust]
+    #df_para_clust = df_para_clust.set_index(df_hosp_pubs['CNES'])
 
     # Mostra gráfico do cotovelo para mostrar a variação da inércia de acordo com o número de clusters
     min_max_num_clusters = range(4, 30)
@@ -45,8 +46,10 @@ if __name__ == '__main__':
     plt.plot(min_max_num_clusters, inercias, '-o')
     plt.xlabel('Número de clusters')
     plt.ylabel('Inércia')
+    plt.title('Gráfico do "cotovelo" para determinação do número de clusters')
+    plt.grid()
     plt.xticks(min_max_num_clusters)
-    arquivo_grafico_inercia = os.path.join(DIRETORIO_DADOS_RESULTADOS, 'kmenas_inercia_vs_k.png')
+    arquivo_grafico_inercia = os.path.join(DIRETORIO_DADOS_RESULTADOS, 'kmeans_inercia_vs_k.png')
     plt.savefig(arquivo_grafico_inercia)
 
     # Número de clusters determinado pela técnica do jolho
@@ -56,14 +59,13 @@ if __name__ == '__main__':
     modelo = KMeans(n_clusters=NUMERO_CLUSTERS, random_state=42)
     modelo.fit(df_para_clust)
 
-    # Adiciona os clusters ao dataframe
-    df_hosp_pubs_oss_com_perfil['CLUSTER'] = modelo.labels_
-    df_para_clust['CLUSTER'] = modelo.labels_ # TODO: remover uma destas variáveis
+    # Adiciona a coluna CLUSTER ao dataframe
+    df_hosp_pubs.insert(loc=1, column='CLUSTER', value=modelo.labels_)
 
-    # Salva planilha original com a coluna CLUSTER adicionar com os respectivos clusters
+    # Salva planilha original com a coluna CLUSTER adicionada
     arquivo_dados_basename = os.path.basename(os.path.splitext(arquivo_para_clusterizacao)[0])
     arquivo_dados_clusterizados = os.path.join(DIRETORIO_DADOS_INTERMEDIARIOS, arquivo_dados_basename + '_clusterizado.xlsx')
-    df_hosp_pubs_oss_com_perfil.to_excel(arquivo_dados_clusterizados, index=False)
+    df_hosp_pubs.to_excel(arquivo_dados_clusterizados, index=False)
 
     """
     A Criação dos clusters termina aqui. Após isto, há somente código para análise da clusterização.
@@ -75,15 +77,12 @@ if __name__ == '__main__':
     df_mapa_subgrupo_desc.index = ['0' + str(codigo) for codigo in df_mapa_subgrupo_desc.index]
     mapa_subgrupo_desc = dict(zip(df_mapa_subgrupo_desc.index, df_mapa_subgrupo_desc.DESCRICAO))
 
-    # Para cada coluna com código de subgrupo, i.e. da 11 até a penúltima (que é a com o id do cluster)
+    # Adiciona a descrição textual ao nome das coluans de procedimentos
     novos_nomes_colunas = {}
-    for coluna in df_hosp_pubs_oss_com_perfil.columns[12:-1]:
-
-        # Cria nomvo nome para a coluna, incluindo código e descrição do subgrupo de procedimentos
-        sistema = 'SIH' if coluna[0] == '1' else 'SIA'
+    for coluna in df_para_clust.columns:
         codigo = coluna[4:8]
         desc = mapa_subgrupo_desc[codigo]
-        novos_nomes_colunas[coluna] = sistema + '-' + codigo + ' - ' + desc
+        novos_nomes_colunas[coluna] = coluna + ' - ' + desc
 
     # Renomeia colunas
     df_para_clust = df_para_clust.rename(columns=novos_nomes_colunas)
@@ -94,8 +93,8 @@ if __name__ == '__main__':
     # Para cada cluster
     for k in range(0, NUMERO_CLUSTERS):
 
-        # Obtém hospitais do cluster  (exclui 1ª e última coluna (CNES e Cluster))
-        df_cluster = df_para_clust[df_para_clust.CLUSTER == k][df_para_clust.columns[1:-1]]
+        # Obtém hospitais do cluster (Repara que no filtro, utilizamos a variável df_hosp_pubs ao invés da df_cluster)
+        df_cluster = df_para_clust[df_hosp_pubs.CLUSTER == k]
 
         # Obtém centroid
         centroid = df_cluster.mean(axis=0)
@@ -111,10 +110,10 @@ if __name__ == '__main__':
         ax.grid()
         ax.set_xlim((0, 1.))
         plt.tight_layout()
-        arquivo_grafico_barras_cluster = os.path.join(DIRETORIO_DADOS_RESULTADOS, 'cluster_{k}.png'.format(k=k))
+        arquivo_grafico_barras_cluster = os.path.join(DIRETORIO_DADOS_INTERMEDIARIOS, 'cluster_{k}.png'.format(k=k))
         plt.savefig(arquivo_grafico_barras_cluster)
 
     # Salva planilha com os centroids
     df_centroids = pd.DataFrame(centroids)
-    arquivo_centroids = os.path.join(DIRETORIO_DADOS_RESULTADOS, 'centroids_kmeans_{k}_clusters.xlsx'.format(k=NUMERO_CLUSTERS))
+    arquivo_centroids = os.path.join(DIRETORIO_DADOS_INTERMEDIARIOS, 'centroids_kmeans_{k}_clusters.xlsx'.format(k=NUMERO_CLUSTERS))
     df_centroids.to_excel(arquivo_centroids)
