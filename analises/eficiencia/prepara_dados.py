a947faefe03d9337e13846b3e630da20758d6879
Saul Campos Berardo <camposb@tcu.gov.br>
2020-03-15 20:04:13 -0300

    Adição de filtros para remoção de unidades com problemas de qualidade de dados e melhorias e remoção de NaNs.

--- analises/eficiencia/prepara_dados.py
+++ analises/eficiencia/prepara_dados.py
@@ -7,6 +7,7 @@ Prepara planilhas para clusterização e análise DEA.
     Exclusão e inclusão de unidades (valores de 2018):
      - Unidades não hospitalares são removidas (sobram 7.745 de um total original de 331.058)
      - Unidades não públicas são removidas (sobram 3.304)
+     - Unidades com valor zero em variável utilizada pela DEA (sobram 2.861 de 3.304)
 """
 
 
@@ -17,7 +18,7 @@ import pandas_profiling
 if __name__ == '__main__':
 
     # Ano dos dados para DEA (para fis de clusterização, estamos sempre utilizando os dados de 2018)
-    ANO = '2019'
+    ANO = '2018'
 
     # Obtem diretório raiz do projeto
     DIRETORIO_RAIZ_PROJETO = os.path.dirname(os.path.realpath(__file__))
@@ -29,6 +30,10 @@ if __name__ == '__main__':
     # Carrega dados da planilha gerada pelo Eric
     arquivo_dados = os.path.join(DIRETORIO_DADOS_ORIGINAIS, 'd{ANO}.csv'.format(ANO=ANO))
     df_tudo = pd.read_csv(arquivo_dados)
+
+    # Substiui nans pelo valor zero nos valores da produção do SIA e do SIH
+    df_tudo.VALOR_SIA = df_tudo.VALOR_SIA.fillna(value=0)
+    df_tudo.VALOR_SIH = df_tudo.VALOR_SIH.fillna(value=0)
 
     # Mapeamento entre primeiro dígito do código da natureza jurídica e o tipo de natureza jurídica
     map_nat_jur = {'1': 'Administração Pública', '2': 'Entidades Empresariais', '3': 'Entidades sem Fins Lucrativos',
@@ -44,6 +49,7 @@ if __name__ == '__main__':
     print('Início', df_tudo.shape)
     tipos_unidades_mantidas = ['HOSPITAL GERAL', 'HOSPITAL ESPECIALIZADO', 'UNIDADE MISTA', 'PRONTO SOCORRO GERAL',
                                'PRONTO SOCORRO ESPECIALIZADO', 'HOSPITAL/DIA - ISOLADO']
+
     # Mantém somente unidades de caráter hospitalar
     df_hosp = df_tudo[df_tudo.TIPO.isin(tipos_unidades_mantidas)]
     print('Após remover não-hospitais', df_hosp.shape)
@@ -62,8 +68,8 @@ if __name__ == '__main__':
     df_hosp_pubs.insert(loc=pos_col_apos_tipo_unidade, column='SE_GERIDA_OSS', value=se_gerida_oss)
 
     # Adiciona a coluna com o valor total da produção do hospital, incluindo SIA e SIH
-    pos_col_antes_valor_sia  = df_hosp_pubs.columns.to_list().index('VALOR_SIA')
-    valor_total =  df_hosp_pubs['VALOR_SIA'] + df_hosp_pubs['VALOR_SIH']
+    pos_col_antes_valor_sia = df_hosp_pubs.columns.to_list().index('VALOR_SIA')
+    valor_total = df_hosp_pubs['VALOR_SIA'] + df_hosp_pubs['VALOR_SIH']
     df_hosp_pubs.insert(loc=pos_col_antes_valor_sia, column='SIA_SIH_VALOR', value=valor_total)
 
     # Renomeia e remove colunas
@@ -83,6 +89,21 @@ if __name__ == '__main__':
         'VALOR_SIA', 'VALOR_SIH', 'QTD_SIA', 'QTD_SIH'
     ])
 
+    # Remove hospitais que possuem valor igual a zero para alguma das variáveis utilizadas na DEA
+    print('Removendo linhas com valor zero nas colunas utilizadas para DEA')
+    colunas_dea = ['CNES_LEITOS_SUS', 'CNES_SALAS', 'CNES_MEDICOS', 'CNES_PROFISSIONAIS_ENFERMAGEM', 'SIA_SIH_VALOR']
+    for coluna in colunas_dea:
+        mascara = df_hosp_pubs[coluna] == 0
+        df_hosp_pubs = df_hosp_pubs[~mascara]
+        print(' * {n} linhas revmovidas por conter zero na coluna {coluna}'.format(n=sum(mascara), coluna=coluna))
+    print('Número de estabelcimentos ao final do processamento: {n}'.format(n=len(df_hosp_pubs)))
+
+    # Verifica se ao final houve algum hospital somatória de proporções de procedimentos inferior a 0.99
+    idx = df_hosp_pubs.columns.to_list().index('SIA-0101')
+    num_com_soma_inferior_a_um = sum(df_hosp_pubs[df_hosp_pubs.columns[idx::]].sum(axis=1) < 0.999)
+    assert(num_com_soma_inferior_a_um == 0)
+
     # Salva planilha para DEA
     arquivo_para_dea = os.path.join(DIRETORIO_DADOS_INTERMEDIARIOS, 'hosp_pubs_{ANO}.xlsx'.format(ANO=ANO))
     df_hosp_pubs.to_excel(arquivo_para_dea, index=False)
+
