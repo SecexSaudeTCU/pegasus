3c9308e9997fbd2c4c6be438b6cc41012f75e357
ericlbc <ericlb@tcu.gov.br>
2020-06-01 15:43:47 -0300

    Update pip version

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import os
 import setuptools
 
 lg = """
@@ -37,26 +38,51 @@ Finally, the data module is installed using the command(on Linux platforms):
 ```$ sudo pip install pegasus-dados```
 """
 
-setuptools.setup(
-    name="pegasus-dados",
-    version="0.1.8",
-    packages=setuptools.find_packages(),
-    package_data={
-        '': ['*.xlsx', '*.c', '*.h', '*.o', '*.so', '*.md']
-    },
-    author="SecexSaudeTCU",
-    author_email="ericlb@tcu.gov.br",
-    description="Plataforma Eletrônica de Governança e Accountability do SUS - Módulo dados",
-    long_description=lg,
-    long_description_content_type="text/markdown",
-    url="https://github.com/SecexSaudeTCU/pegasus/tree/master/pegasus/dados",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License"
-    ],
-    python_requires='>=3.6',
-    setup_requires=['cffi>=1.0.0'],
-    cffi_modules=["pegasus/dados/transform/extract/_build_readdbc.py:ffibuilder"],
-    install_requires=['numpy', 'pandas', 'xlrd', 'dbfread', 'cffi>=1.0.0',
-                      'pyarrow', 'fastparquet', 'psycopg2', 'SQLAlchemy']
-)
+if os.name == 'posix':
+
+    setuptools.setup(
+        name="pegasus-dados",
+        version="0.2.0",
+        packages=setuptools.find_packages(),
+        package_data={
+            '': ['*.xlsx', '*.c', '*.h', '*.o', '*.so', '*.md']
+        },
+        author="SecexSaudeTCU",
+        author_email="ericlb@tcu.gov.br",
+        description="Plataforma Eletrônica de Governança e Accountability do SUS - Módulo dados",
+        long_description=lg,
+        long_description_content_type="text/markdown",
+        url="https://github.com/SecexSaudeTCU/pegasus/tree/master/pegasus/dados",
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License"
+        ],
+        python_requires='>=3.6',
+        setup_requires=['cffi>=1.0.0'],
+        cffi_modules=["pegasus/dados/transform/extract/_build_readdbc.py:ffibuilder"],
+        install_requires=['numpy', 'pandas', 'xlrd', 'dbfread', 'cffi>=1.0.0',
+                          'pyarrow', 'fastparquet', 'psycopg2', 'SQLAlchemy']
+    )
+
+elif os.name == 'nt':
+
+    setuptools.setup(
+        name="pegasus-dados",
+        version="0.2.0",
+        packages=setuptools.find_packages(),
+        package_data={
+            '': ['*.xlsx', '*.c', '*.h', '*.o', '*.so', '*.md']
+        },
+        author="SecexSaudeTCU",
+        author_email="ericlb@tcu.gov.br",
+        description="Plataforma Eletrônica de Governança e Accountability do SUS - Módulo dados",
+        long_description=lg,
+        long_description_content_type="text/markdown",
+        url="https://github.com/SecexSaudeTCU/pegasus/tree/master/pegasus/dados",
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License"
+        ],
+        python_requires='>=3.6',
+        install_requires=['numpy', 'pandas', 'xlrd', 'dbfread', 'pyarrow', 'fastparquet', 'psycopg2', 'SQLAlchemy']
+    )
